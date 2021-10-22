8085ab05dcdbf280ec9ffd7ff30ac51360383a48
oandrushko <oandrushko@imolainformatica.it>
2021-10-23 00:59:40 +0200

    Add scraper service

--- scraper/src/app.ts
+++ scraper/src/app.ts
@@ -1,1 +1,20 @@
-console.log("Hello there!");
+import { performance } from 'perf_hooks';
+import { Meme } from './model/Meme';
+import { IScraper, Scraper } from './services/Scraper';
+
+const scraper: IScraper = new Scraper();
+
+(async () => {
+    const start = performance.now();
+
+    const result: Meme = await scraper.scrapSingle(
+        process.env.SCRAP_TARGET,
+        process.env.SCRAP_TEXT_PATTERN,
+        process.env.SCRAP_CONTENT_PATTERN,
+        process.env.SCRAP_CONTENT_PATTERN_ATTR
+    );
+
+    const end = performance.now();
+    console.log(`Execution time: ${end - start} ms (${(end - start) / 1000} s)`);
+    console.log(result);
+})();
