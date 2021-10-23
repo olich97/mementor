88e496b64e63a29fdb6c2430757a0d54b8279dc4
oandrushko <oandrushko@imolainformatica.it>
2021-10-23 16:25:56 +0200

    feat(scraper): add multipage scrapping

--- scraper/src/app.ts
+++ scraper/src/app.ts
@@ -1,7 +1,7 @@
 import { performance } from 'perf_hooks';
 import { Meme } from './model/Meme';
 import { ScrapConfiguration } from './model/ScrapConfiguration';
-import { IScraper, Scraper } from './services/PageScrapper';
+import { IScraper, Scraper } from './services/Scrapper';
 
 const scrapConfig: ScrapConfiguration = new ScrapConfiguration(
     process.env.SCRAP_TARGET_ELEMENT_PATTERN,
@@ -15,9 +15,17 @@ const scraper: IScraper = new Scraper(scrapConfig);
 (async () => {
     const start = performance.now();
 
-    const result: Meme[] = await scraper.scrap(process.env.SCRAP_TARGET);
+    //const result: Meme[] = await scraper.scrap(process.env.SCRAP_TARGET);
+
+    for (let pageNumber = 0; pageNumber < 6; pageNumber++) {
+        const url: string = process.env.SCRAP_TARGET + '/' + pageNumber;
+        console.log(`Scrapping url: ${url}`);
+        const result: Meme[] = await scraper.scrap(url);
+        console.log('Result: ');
+        console.log(result);
+        console.log('------------------');
+    }
 
     const end = performance.now();
     console.log(`Execution time: ${end - start} ms (${(end - start) / 1000} s)`);
-    console.log(result);
 })();
