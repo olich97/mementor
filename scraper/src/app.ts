73e742c9587c7de25122703f57bff598a2b22d04
olich97 <olich.me@gmail.com>
2021-11-08 20:55:36 +0100

    rafactor(scraper): separated main functions

--- scraper/src/app.ts
+++ scraper/src/app.ts
@@ -35,64 +35,67 @@ async function inizializeDatabase() {
     memeRepository = connection.getRepository(Meme);
 }
 
+async function elaboratePage(url: string) {
+    try {
+        console.log(`Scrapping url: ${url}`);
+        const memes: Meme[] = await scraper.scrap(url);
+        console.log('Scrapping complete');
+        //console.log(memes);
+        console.log('------------------');
+        for (let i = 0; i < memes.length; i++) {
+            const newMeme = memes[i];
+            const result = await memeRepository.findOne({ code: newMeme.code });
+            //console.log('Result ', result);
+            if (!result) {
+                // need to check if there already exist content to associate meme with
+                const existingMemeContent = await memeRepository.findOne({
+                    relations: ['content'],
+                    where: {
+                        content: {
+                            hash: newMeme.content.hash
+                        }
+                    }
+                });
+                if (existingMemeContent) {
+                    // just map map content to new meme
+                    newMeme.content = existingMemeContent.content;
+                } else {
+                    // need to be uploaded to external storage
+                    const fileName = newMeme.content.sourceUrl.substring(
+                        newMeme.content.sourceUrl.lastIndexOf('/') + 1
+                    );
+                    const skyKey = await skynet.uploadFile(newMeme.content.sourceUrl, fileName);
+                    console.log(`Upload successful, skylink: ${skyKey}`);
+                    newMeme.content.storageKey = skyKey;
+                }
+                memeRepository.save(newMeme);
+                console.log('Saved: ', newMeme);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        errorRepository.save(ScrapError.Create(url, <string>error));
+    }
+}
+
+async function elaborateMultiplePages(start: number, end: number) {
+    console.log(`Start scrapping data from page ${start} to page ${end}`);
+    for (let currentPage = start; currentPage < end; currentPage++) {
+        const url: string = process.env.SCRAP_TARGET + '/' + currentPage;
+        await elaboratePage(url);
+    }
+}
+
 (async () => {
     const start = performance.now();
     // create database connection
     await inizializeDatabase();
 
-    for (let pageNumber = 1; pageNumber < 30; pageNumber++) {
-        const url: string = process.env.SCRAP_TARGET + '/' + pageNumber;
-        try {
-            console.log(`Scrapping url: ${url}`);
-            const memes: Meme[] = await scraper.scrap(url);
-            console.log('Scrapping complete');
-            //console.log(memes);
-            console.log('------------------');
-            for (let i = 0; i < memes.length; i++) {
-                const newMeme = memes[i];
-                const result = await memeRepository.findOne({ code: newMeme.code });
-                //console.log('Result ', result);
-                if (!result) {
-                    // need to check if there already exist content to associate meme with
-                    const existingMemeContent = await memeRepository.findOne({
-                        relations: ['content'],
-                        where: {
-                            content: {
-                                hash: newMeme.content.hash
-                            }
-                        }
-                    });
-                    if (existingMemeContent) {
-                        // just map map content to new meme
-                        newMeme.content = existingMemeContent.content;
-                    } else {
-                        // need to be uploaded to external storage
-                        const fileName = newMeme.content.sourceUrl.substring(
-                            newMeme.content.sourceUrl.lastIndexOf('/') + 1
-                        );
-                        const skyKey = await skynet.uploadFile(newMeme.content.sourceUrl, fileName);
-                        console.log(`Upload successful, skylink: ${skyKey}`);
-                        newMeme.content.storageKey = skyKey;
-                    }
-                    memeRepository.save(newMeme);
-                    console.log('Saved: ', newMeme);
-                }
-            }
-        } catch (error) {
-            console.error(error);
-            errorRepository.save(ScrapError.Create(url, <string>error));
-        }
-    }
+    await elaborateMultiplePages(
+        parseInt(process.env.SCRAP_START_PAGE),
+        parseInt(process.env.SCRAP_END_PAGE)
+    );
 
     const end = performance.now();
     console.log(`Execution time: ${end - start} ms (${(end - start) / 1000} s)`);
 })();
-
-function toArrayBuffer(buffer) {
-    const ab = new ArrayBuffer(buffer.length);
-    const view = new Uint8Array(ab);
-    for (let i = 0; i < buffer.length; ++i) {
-        view[i] = buffer[i];
-    }
-    return ab;
-}
