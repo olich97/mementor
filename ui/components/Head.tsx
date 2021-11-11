3cd4c45cacf26584f2fdb7e34677cf492047884f
oandrushko <oandrushko@imolainformatica.it>
2021-11-12 00:27:48 +0100

    feat(ui): initial project structure

---
+++ ui/components/Head.tsx
@@ -0,0 +1,36 @@
+import NextHead from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { MetaProps } from '../types/layout';
+
+export const WEBSITE_HOST_URL = 'https://nextjs-typescript-mdx-blog.vercel.app';
+const siteTitle = 'Mementor - Fun for Dev';
+const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
+  const router = useRouter();
+  const meta: MetaProps = {
+    title: siteTitle,
+    description: 'Some funny memes for developers',
+    image: `${WEBSITE_HOST_URL}/images/site-preview.png`,
+    type: 'website',
+    ...customMeta,
+  };
+
+  return (
+    <NextHead>
+      <title>{meta.title}</title>
+      <meta content={meta.description} name="description" />
+      <meta property="og:url" content={`${WEBSITE_HOST_URL}${router.asPath}`} />
+      <link rel="canonical" href={`${WEBSITE_HOST_URL}${router.asPath}`} />
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={meta.title} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+      <meta property="og:image" content={meta.image} />
+      {meta.date && (
+        <meta property="article:published_time" content={meta.date} />
+      )}
+    </NextHead>
+  );
+};
+
+export default Head;
