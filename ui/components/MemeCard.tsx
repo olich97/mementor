91a54c72daf485751d9d44f7e1b84cec355a7c23
oandrushko <oandrushko@imolainformatica.it>
2021-11-12 20:11:11 +0100

    feat(ui): add pagination and search

--- ui/components/MemeCard.tsx
+++ ui/components/MemeCard.tsx
@@ -3,15 +3,20 @@ import { MemeType } from '../types/meme';
 
 const MemeCard = ({ meme }: { meme?: MemeType }): JSX.Element => {
   return (
-    <div className="rounded-lg shadow-xl">
-      <header className=" text-xl font-extrabold p-4">{meme.text}</header>
-      <img src={meme.content} alt={meme.code} className="w-full object-cover" />
-      <br></br>
-      <footer className="text-right py-3 px-8 text-gray-500">
-        <button className="py-2 px-4 mt-5 bg-green-500 rounded-lg text-white font-semibold hover:bg-green-600">
-          GET STARTED
-        </button>
-      </footer>
+    <div className="flow-root">
+      <div className="box-border md:box-content">
+        <header className=" text-xl font-extrabold py-4">{meme.text}</header>
+        <img
+          src={meme.content}
+          alt={meme.code}
+          className="w-full md:w-7/12 lg:w-9/12"
+        />
+        <footer className="text-right px-1 text-gray-500" hidden>
+          <button className="py-2 px-4 mt-5 bg-green-500 rounded-lg text-white font-semibold hover:bg-green-600">
+            GET STARTED
+          </button>
+        </footer>
+      </div>
     </div>
   );
 };
--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,22 +1,85 @@
 import { GetStaticProps } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import { getAllMemes } from '../lib/api';
-import { MemeType } from '../types/meme';
 import MemeCard from '../components/MemeCard';
+import Pagination from '../components/Pagination';
 
-type IndexProps = {
-  memes: MemeType[];
-};
+export const Index = (): JSX.Element => {
+  const [posts, setPosts] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
 
-export const Index = ({ memes }: IndexProps): JSX.Element => {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = getAllMemes(['id', 'code', 'text', 'content']);
+      setPosts(res);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  let currentPosts; //= posts.slice(indexOfFirstPost, indexOfLastPost);
+  let filteredPosts = posts;
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // search handling
+  if (searchValue.length > 0) {
+    filteredPosts = posts.filter((item) =>
+      item.text.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
+  } else {
+    currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  }
+
   return (
     <Layout>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-        {memes.map((meme) => (
+      <div className="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 mr-2 text-gray-600"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+        <input
+          type="text"
+          name="Meme"
+          autoFocus
+          placeholder="Search memes .... "
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="w-1/3 py-2 border-b-2 outline-none focus:border-green-400"
+        />
+      </div>
+      <div className="space-y-4 divide-y md:divide-y-4">
+        {currentPosts.map((meme) => (
           <MemeCard key={meme.id} meme={meme} />
         ))}
       </div>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={filteredPosts.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </Layout>
   );
 };
