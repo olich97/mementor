d107cc35466c03d0371f686879ed27c4aaa32706
oandrushko <oandrushko@imolainformatica.it>
2021-11-15 15:41:20 +0100

    refactor(api): add entities base  mapping in MemeService

--- api/src/infrastructure/StorageService.ts
+++ api/src/infrastructure/StorageService.ts
@@ -1,7 +1,7 @@
 import { IStorageService } from '@/core/interfaces/services/IStorageService';
-
+import config from './AppSettings';
 export class StorageService implements IStorageService {
   async getUrl(key: string): Promise<string> {
-    return `https://siasky.net/${key}`;
+    return `${config.storage.url}/${key}`;
   }
 }
