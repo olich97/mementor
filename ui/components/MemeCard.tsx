b0486179b985fe5c0ba3d1aad60b29a5c5501395
olich97 <olich.me@gmail.com>
2021-11-16 09:05:38 +0100

    feat(ui): change some styles

--- ui/components/MemeCard.tsx
+++ ui/components/MemeCard.tsx
@@ -6,12 +6,15 @@ const MemeCard = ({ meme }: { meme?: MemeType }): JSX.Element => {
   return (
     <div className="flow-root">
       <div className="box-border md:box-content">
-        <Link href={{ pathname: '/memes/[code]', query: { code: meme.code } }}>
-          <div className="cursor-pointer">
-            <header className=" text-xl font-extrabold py-4">{meme.text}</header>
-            <img src={meme.contentUrl} alt={meme.code} className="w-full md:w-7/12 lg:w-9/12" />
-          </div>
-        </Link>
+        <div>
+          <Link href={{ pathname: '/memes/[code]', query: { code: meme.code } }}>
+            <header className="cursor-pointer text-xl font-extrabold py-4">{meme.text}</header>
+          </Link>
+          <Link href={{ pathname: '/memes/[code]', query: { code: meme.code } }}>
+            <img src={meme.contentUrl} alt={meme.code} className="w-full md:w-7/12 lg:w-9/12 cursor-pointer" />
+          </Link>
+        </div>
+
         <footer className="text-right px-1 text-gray-500" hidden>
           <button className="py-2 px-4 mt-5 bg-green-500 rounded-lg text-white font-semibold hover:bg-green-600">
             GET STARTED
